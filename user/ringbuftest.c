5612e6f3b65461e2b068e691321948cde04aef81
PRANAV ASHOK SHENOY <u1365432@lab1-1.eng.utah.edu>
2021-10-02 19:00:00 -0600

    verifying values are mapped correctly

--- user/ringbuftest.c
+++ user/ringbuftest.c
@@ -7,15 +7,19 @@ void test_find_0() {
 
   uint64 vm_addr;
   ringbuf("pranav", &vm_addr, 0);
-  printf("Address received to caller %p\n", vm_addr);
   uint64* start = (uint64*) vm_addr;
+  printf("Address received to caller %p, start: %p\n", vm_addr, start);
+  *start = (uint64) 443;
+  
+
   for(int i=0; i<16;i++) {
-    *(start+i*4096) = i*10;
+    *(start+i*512) = i*10;
   }
 
   for(int i=0;i<32;i++) {
-    printf("i: %d address: %p value: %d\n", i, start+i*4096, *(start+i*4096));
+    printf("i: %d address: %p value: %d\n", i, start+i*512, *(start+i*512));
   }
+
 }
 
 void test_find_1() {
