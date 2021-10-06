626d5880a1efa9185bf45a95742bf2b085b81be2
hkjl10201 <hkjl10201@gmail.com>
2021-10-05 22:03:16 -0600

    userRingbufLib

--- user/ringbuf.c
+++ user/ringbuf.c
@@ -1,36 +1,131 @@
 #include "ringbuf.h"
 
-struct user_ring_buf {
-  void *buf;
-  void *book;
-  int exists;
+struct ringbook
+{
+  uint64* read;
+  uint64* write;
 };
 
-void store(int *p, int v) {
+struct user_ring_buf {
+  char *name;
+  uint64* addr;
+  int exists;
+}user_ring_bufs[MAX_RINGBUFS];
+
+// It writes v into *p.
+void store(uint64 *p, uint64 v) {
   __atomic_store_8(p, v, __ATOMIC_SEQ_CST);
 }
 
-int load(int *p) {
+// It returns the contents of *p.
+int load(uint64 *p) {
   return __atomic_load_8(p, __ATOMIC_SEQ_CST);
 }
 
-//- invokes sys_ringbuf(name, 0)
-//- return -1 on fail
-//- otherwise return ring buffer descriptor
+// find the position of ring buffer by name
+int find(char* name) {
+    int index=-1;
+    for(int i=0;i<MAX_RINGBUFS;i++) {
+        if(strncmp(name, user_ring_bufs[i].name, strlen(name)) == 0) {
+            if(user_ring_bufs[i].exists==1)
+                index=i;
+        }
+    }
+    return index;
+}
+
+// invokes ringbuf(name, addr, 0)
+// return -1 on fail
+// otherwise return ring buffer descriptor
 int create_ring_buffer(char *name) {
-
+    uint64 vm_addr;
+    int res = ringbuf(name,&vm_addr,0);
+    if(res==-1) {
+        return -1;
+    }
+    uint64* start = (uint64*) vm_addr;
+    struct ringbook book;
+    book.read=start;
+    book.write=start;
+    *(start+32*512) = &book;
+    int empty_slot=-1;
+    for(int i=0;i<MAX_RINGBUFS;i++){
+        if(user_ring_bufs[i].exists==0){
+            user_ring_bufs[i].name=name;
+            user_ring_bufs[i].addr=start;
+            user_ring_bufs[i].exists=1;
+            empty_slot=i;
+            break;
+        }
+    }
+    if(empty_slot==-1){
+      printf("Ring buffers are full.\n");
+      return -1;
+    } else {
+      return empty_slot;
+    }
 }
 
-//- invokes sys_ringbuf(name, 1)
-//- return -1 on fail
-int delete_ring_buffer(int ring_buffer_descriptor){
-
+// invokes create_ringbuf(name, addr, 1)
+// return -1 on fail
+int delete_ring_buffer(char *name){
+    int index=find(name);
+    if(index==-1){
+      printf("Ring buffer %s not found.\n",name);
+      return -1;
+    }
+    int res = create_ringbuf(name,user_ring_bufs[index].addr,1);
+    if(res==-1) {
+      return -1;
+    }
+    user_ring_bufs[index].exists=0;
+    return 0;
 }
 
-void ringbuf_start_read(int ring_desc, char **addr, int *bytes) {
-
+void ringbuf_start_read(char *name, uint64 **addr, int *bytes) {
+    int index=find(name);
+    if(index==-1){
+      printf("Ring buffer %s not found.\n",name);
+      return;
+    }
+    uint64* start=user_ring_bufs[index].addr;
+    uint64 read=load(start+32*512);   //  load 'read' from 'book' 
+    uint64 write=load(start+32*512+8);    // load 'write' from 'book'
+    addr=read;
+    bytes=(write-read)%(RINGBUF_SIZE*PG_SIZE);
 }
-void ringbuf_finish_read(int ring_desc, int bytes);
 
-void ringbuf_start_write(int ring_desc, char **addr, int *bytes);
-void ringbuf_finish_write(int ring_desc, int bytes);
+void ringbuf_finish_read(char *name, int bytes) {
+    int index=find(name);
+    if(index==-1){
+      printf("Ring buffer %s not found.\n",name);
+      return;
+    }
+    uint64* start=user_ring_bufs[index].addr;
+    uint64 read=load(start+32*512);   //  load 'read' from 'book' 
+    store(start+32*512,(read+bytes)%(RINGBUF_SIZE*PG_SIZE));
+}
+
+void ringbuf_start_write(char *name, uint64 **addr, int *bytes) {
+    int index=find(name);
+    if(index==-1){
+      printf("Ring buffer %s not found.\n",name);
+      return;
+    }
+    uint64* start=user_ring_bufs[index].addr;
+    uint64 read=load(start+32*512);   //  load 'read' from 'book' 
+    uint64 write=load(start+32*512+8);    // load 'write' from 'book'
+    addr=write;
+    bytes=(read-write)%(RINGBUF_SIZE*PG_SIZE);
+}
+
+void ringbuf_finish_write(char *name, int bytes) {
+    int index=find(name);
+    if(index==-1){
+      printf("Ring buffer %s not found.\n",name);
+      return;
+    }
+    uint64* start=user_ring_bufs[index].addr;
+    uint64 write=load(start+32*512+8);    // load 'write' from 'book'
+    store(start+32*512+8,(write+bytes)%(RINGBUF_SIZE*PG_SIZE));
+}
--- user/ringbuf.h
+++ user/ringbuf.h
@@ -1,15 +1,20 @@
+#include "user.h"
+#include "../kernel/types.h"
 
+#define RINGBUF_SIZE 16
+#define MAX_RINGBUFS 10
+#define PG_SIZE 4096
 
 int create_ring_buffer(char *name);
 
-int delete_ring_buffer(int ring_buffer_descriptor);
+int delete_ring_buffer(char *name);
 
 void store(int *p, int v);
 
 int load(int *p);
 
-void ringbuf_start_read(int ring_desc, char **addr, int *bytes);
-void ringbuf_finish_read(int ring_desc, int bytes);
+void ringbuf_start_read(char *name, uint64 **addr, int *bytes);
+void ringbuf_finish_read(char *name, int bytes);
 
-void ringbuf_start_write(int ring_desc, char **addr, int *bytes);
-void ringbuf_finish_write(int ring_desc, int bytes);
+void ringbuf_start_write(char *name, uint64 **addr, int *bytes);
+void ringbuf_finish_write(char *name, int bytes);
