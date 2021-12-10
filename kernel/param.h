ad319fac19c7d96500edff506f3494edecf5958d
Pranav Shenoy <pranav.shenoy@utah.edu>
2021-12-10 02:30:47 -0700

    increasing buf

--- kernel/param.h
+++ kernel/param.h
@@ -9,6 +9,6 @@
 #define MAXOPBLOCKS  10  // max # of blocks any FS op writes
 #define NPARALLELLOGGING 4
 #define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
-#define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
+#define NBUF         (MAXOPBLOCKS*3*4)  // size of disk block cache
 #define FSSIZE       1000  // size of file system in blocks
 #define MAXPATH      128   // maximum file path name
