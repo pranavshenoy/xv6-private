6bfb5eed0c055ede0c09141d1c6dfaf5007736bf
Pranav Shenoy <pranav.shenoy@utah.edu>
2021-12-10 02:58:39 -0700

    mnr

--- kernel/log.c
+++ kernel/log.c
@@ -369,14 +369,15 @@ end_op(void)
 	if(log[INDEX(id)].committing) {
 		panic("The current log struct is already committing\n");
 	}
-	if(log[INDEX(id)].lh.n == 0) {
-		printf("Nothing to write\n");
-		release(&log[INDEX(id)].lock);
-		return;
-	}
+
 	log[INDEX(id)].outstanding -= 1;
 	if(log[INDEX(id)].outstanding != 0) { //not last end_op
 		wakeup(&commit_idx_lk);
+		release(&log[INDEX(id)].lock);
+		return;
+	}
+	if(log[INDEX(id)].lh.n == 0) {
+		printf("Nothing to write\n");
 		release(&log[INDEX(id)].lock);
 		return;
 	}
